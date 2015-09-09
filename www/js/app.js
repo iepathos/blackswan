ead7a0a713b7408da4427b29043d7dcaa707ab85
Glen Baker <iepathos@gmail.com>
2015-09-08 21:10:17 -0400

    Code cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,6 @@
 // Black Swan Notification Improvements by Mocha Dick
 var debug = true;
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
+
 angular.module('blackswan', ['ionic', 'ngCordova'])
 
 .controller('ContactsCtrl', function($scope, $cordovaContacts) {
